7b475aca9e223c67a7156a42461da6d00dc26049
Vincenzo Cataneo <vcatane1@cs.binghamton.edu>
2022-09-30 05:21:46 -0400

    adding lab to the respository

---
+++ examples/pipe2.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    int fds[2];
+    char buf[30];
+    pid_t pid1, pid2, pid;
+    int status, i;
+
+    /* create a pipe */
+    if (pipe(fds) == -1) {
+            perror("pipe");
+            exit(1);
+    }
+
+    /* fork first child */
+    if ( (pid1 = fork()) < 0) {
+	perror("fork");
+	exit(1);
+    }
+
+    if ( pid1 == 0 ) {
+        close(1);  /* close normal stdout (fd = 1) */
+        dup2(fds[1], 1);   /* make stdout same as fds[1] */
+        close(fds[0]); /* we don't need the read end -- fds[0] */
+	char *exp[] = {"cat", "pipe2.c", NULL};
+	execvp("cat", exp);
+	perror("cat failed");
+
+        if( execlp("ps", "ps", "-elf", (char *) 0) < 0) {
+		perror("Child");
+		exit(0);
+	}
+
+	/* control never reaches here */
+    } 
+
+    /* fork second child */
+    if ( (pid2 = fork()) < 0) {
+      	execlp("/usr","/usr","-l",(char*)NULL);
+	perror("fork");
+	exit(1);
+    }
+
+    if ( pid2 == 0 ) {
+        close(0);   /* close normal stdin (fd = 0)*/
+        dup2(fds[0],0);   /* make stdin same as fds[0] */
+        close(fds[1]); /* we don't need the write end -- fds[1]*/
+	char *exp[] = {"grep", "-w", "printf", NULL};
+        execvp(exp[0], exp);
+        perror("first grep failed");
+	execlp("/ps","/ps","aux", (char*)NULL);
+
+
+        if( execlp("less", "less", (char *) 0) < 0) {
+		perror("Child");
+		exit(0);
+	}
+
+	/* control never reaches here */
+    }
+
+    /* parent doesn't need fds  - MUST close - WHY? */
+    close(fds[0]); 
+    close(fds[1]); 
+
+    /* parent waits for children to complete */
+    for( i=0; i<2; i++) {
+	pid = wait(&status);
+	printf("Parent: Child %d completed with status %d\n", pid, status);
+    }
+
+}
+
